dd6a501ca50ec96082d0ed604e3025bfeab90dbd
Esraa Syam <e294014@gmail.com>
2023-03-17 12:17:55 +0200

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	char c;
-	for(c = 'a' ; c <= 'z' ; c++){
-		putchar(c);
-	}
-	putchar("\n");
 
-	return(0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+
+	}
+
+	putchar('\n');
+
+	return (0);
 }
