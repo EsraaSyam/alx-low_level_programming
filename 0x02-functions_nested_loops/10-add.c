fb0fe8e0259c1fcfac909717fdd087eb8a8236f7
Esraa Syam <e294014@gmail.com>
2023-03-21 20:37:23 +0200

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - print the summiton of two numbers.
+ * @n: first num.
+ * @m: second num.
+ * Return: sum of two numbers.
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
