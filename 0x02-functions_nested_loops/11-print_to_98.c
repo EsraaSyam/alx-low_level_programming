e1e9c2e7207a414bab3bb744e193192f82eb491f
Esraa Syam <e294014@gmail.com>
2023-03-21 20:59:31 +0200

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,32 +6,16 @@
  * @n: number.
  */
 
+
 void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			_putchar(i + '0');
-			if (i == 98)
-				continue;
-			printf(", ");
-		}
-	}
-	else if (n > 98)
-	{
-		for (i = n ; i >= 98 ; i--)
-		{
-			_putchar(i + '0');
-			if (i == 98)
-				continue;
-			printf(", ");
-		}
-	}
+	if (n > 98)
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
 	else
-		printf("98");
-
-	_putchar('\n');
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	printf("98\n");
 }
