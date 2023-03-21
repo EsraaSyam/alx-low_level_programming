8477bc825f720b2cd8266254ef1e59af370d273b
Esraa Syam <e294014@gmail.com>
2023-03-21 20:52:11 +0200

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			_putchar(i + '0');
 			if (i == 98)
 				continue;
-			_putchar(',');
+			printf(", ");
 		}
 	}
 	else
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			_putchar(i + '0');
 			if (i == 98)
 				continue;
-			_putchar(',');
+			printf(", ");
 		}
 	}
 
