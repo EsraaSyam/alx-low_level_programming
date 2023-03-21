92e6479994bd14567675c965732159fd58fdfd30
Esraa Syam <e294014@gmail.com>
2023-03-21 11:14:25 +0200

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int c = 0;
+
+	for (c = 0; c < 8; c++)
+		_putchar (str[i]);
+	_putchar ('\n');
+	return (0);
+}
