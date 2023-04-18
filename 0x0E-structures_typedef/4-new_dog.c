4249a51e015ab2568f1ff233dc8f1155b4a32291
Esraa Syam <e294014@gmail.com>
2023-04-18 02:10:36 +0200

    update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+}
