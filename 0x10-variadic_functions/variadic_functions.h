daf3a7dad361daf4abfee5cc07e38e3dff898cb7
Esraa Syam <e294014@gmail.com>
2023-04-22 02:58:54 +0200

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,4 +16,5 @@ void (*f)(va_list);
 } print_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
