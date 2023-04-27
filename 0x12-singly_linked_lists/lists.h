e5fe1f7b6ec86f90dc475afebf5f391ecd474a97
Esraa Syam <e294014@gmail.com>
2023-04-28 02:15:00 +0300

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
@@ -11,12 +12,12 @@
  * Description: singly linked list node struct
  * for Holberton project
  */
-
 typedef struct listint_s
 {
-	int n;
+	char *str;
+	unsigned int len;
 	struct listint_s *next;
-} listint_t;
+} list_t;
 
 /* Prototypes */
 size_t print_list(const list_t *h);
