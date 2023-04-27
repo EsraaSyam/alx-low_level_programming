3642113a2049d0f9397c8cbcd2a57d3ee97020fb
Esraa Syam <e294014@gmail.com>
2023-04-28 02:09:12 +0300

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,16 +13,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if ((*h).str == NULL)
+		if (h -> str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
-			printf('[');
-			printf("%d", (*h).len);
-			printf(']');
-			printf(" %s\n", (*h).str);
-		}
-		h = (*h).next;
+			printf("[%d] %s\n", h -> len, h -> str);
+		h = h -> next;
 		count++;
 	}
 	return (count);
